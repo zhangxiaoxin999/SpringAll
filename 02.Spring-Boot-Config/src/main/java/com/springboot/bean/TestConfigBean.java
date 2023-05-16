d2b291952ac8ec31bd8acf6177fee36f166db266
10224235 <zhangxiaoxin-hq@boe.com.cn>
2023-05-16 14:15:00 +0800

    主要修改pom.xml文件中的Java版本

--- 02.Spring-Boot-Config/src/main/java/com/springboot/bean/TestConfigBean.java
+++ 02.Spring-Boot-Config/src/main/java/com/springboot/bean/TestConfigBean.java
@@ -6,23 +6,27 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Configuration
-@ConfigurationProperties(prefix="test")
+@ConfigurationProperties(prefix = "test")
 @PropertySource("classpath:test.properties")
 @Component
 public class TestConfigBean {
-	private String name;
-	private int age;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
